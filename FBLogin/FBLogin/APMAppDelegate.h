141dfad1e305167f51e13db438c02ef61a17fdc9
Ana Mei <peishi@seas.upenn.edu>
2013-05-06 23:06:24 -0700

    add

---
+++ FBLogin/FBLogin/APMAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  APMAppDelegate.h
+//  FBLogin
+//
+//  Created by Ana Mei on 5/5/13.
+//  Copyright (c) 2013 Ana Mei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface APMAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
